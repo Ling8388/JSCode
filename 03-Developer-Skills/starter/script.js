121a9ff62608c2d173871c35a3df6f13b57b9252
meilingjiang <lingeleven11@gmail.com>
2023-02-06 16:23:07 -0500

    add message

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,20 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    value: Number(prompt('Degrees celsius: ')), //the type is number
+  };
+
+  //   console.log(measurement);
+  //   console.warn(measurement);
+  //   console.error(measurement);
+  //   console.table(measurement);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
